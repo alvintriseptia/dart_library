65e0d8e8351a224254a1fd4f01ebc30ed69a57e1
alvintriseptia <alvintryseptia@gmail.com>
2022-04-13 21:32:45 +0700

    create simple library

---
+++ example/hello.dart
@@ -0,0 +1,7 @@
+import 'package:dart_library/hello.dart';
+
+void main() {
+  print(sayHello("Alvin"));
+
+  Customer customer = Customer();
+}
---
+++ lib/hello.dart
@@ -0,0 +1,6 @@
+library dart_library;
+
+//mengekspor semua isi dari file
+export 'src/say_hello.dart';
+//mengekspor method/class khusus
+export 'src/customer.dart' show Customer;
